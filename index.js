f070fb620492b644faaba4a373a3ff74e2eb8f95
Markus Bertling <markus.bertling@jimdo.com>
2018-05-11 18:48:19 +0200

    chore: cleanup issue comment posting which was a test

--- index.js
+++ index.js
@@ -5,13 +5,4 @@ module.exports = robot => {
     ["pull_requests.opened", "pull_requests.reopened"],
     handleNewPullRequest
   );
-  robot.on("issues.opened", async context => {
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = context.issue({ body: "Hello World!" });
-    robot.log(context.issue);
-    // Post a comment on the issue
-    return context.github.issues.createComment(params);
-  });
 };
